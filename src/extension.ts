77f0069bb874acc88689c6accea585c89bb42f85
coffeemil <ecalm5@gmail.com>
2024-11-20 15:18:56 +0800

    feat: finish get LineText

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+interface Editor {
+  editor: "";
+}
+
+// Get Text	Line
+function getTextEditorCurrentLine(): string {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const position = editor.selection.active;
+    const line = position.line;
+    const nextLinePosition = position.with(line + 1, 0);
+    const lineContent = editor.document.lineAt(line).text;
+    return lineContent;
+  } else {
+    return "";
+  }
+}
+
+// request API
+function askAI(comment: string): string {
+  return "这是AI回复";
+}
+
+// output AI Code
+function outputCode(AIResponse: string) {
+  vscode.window.showInformationMessage(`调用输出${AIResponse}`);
+
+  //   editBuilder.insert(
+  //     linePosition + 1,
+  //     `Current line content: ${lineContent}\n`
+  //   );
+  console.log("调用输出");
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "zen.getCurrentLineContent",
+    () => {
+      // Get the active editor
+      const lineContent = getTextEditorCurrentLine();
+      if (!["	"].includes(lineContent) && lineContent.length > 0) {
+        const AIResponse = askAI(lineContent);
+        outputCode(AIResponse);
+      }
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zen" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zen.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zen!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
