846ed8beea8490bda5496dc4774d4e2c6fa633da
coffeemil <ecalm5@gmail.com>
2024-11-21 13:19:26 +0800

    feat: finish AIask

--- src/extension.ts
+++ src/extension.ts
@@ -22,13 +22,60 @@ function getEditor(): Editor | null {
 }
 
 // request API
-function askAI(comment: string): string {
-  return "这是AI回复";
+async function askAI(question: string): Promise<string> {
+  const api_key = "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";
+
+  const params = {
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an automatic code writing program,Returns only the js code"
+      },
+      {
+        role: "user",
+        content: "Returns only the js code , return function"
+      },
+      {
+        role: "user",
+        content: "No special characters"
+      },
+      {
+        role: "user",
+        content: question
+      }
+    ],
+    model: "gpt-3.5-turbo",
+    temperature: 0.7, // random
+    max_tokens: 4096 // max token
+  };
+
+  const res = await fetch("https://aigptx.top/v1/chat/completions", {
+    method: "post",
+    body: JSON.stringify(params),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + api_key
+    }
+  });
+  if (!res.ok) {
+    throw new Error("Net Error");
+  }
+
+  // response = requests.post(
+  //     "https://aigptx.top/v1/chat/completions",
+  //     headers=headers,
+  //     json=params,
+  //     stream=False
+  // )
+  const data: any = await res.json();
+  const result = data["choices"][0]["message"]["content"];
+
+  return result;
 }
 
 // output AI Code
 function outputCode(editor: Editor, AIResponse: string) {
-  vscode.window.showInformationMessage(`调用输出${AIResponse}`);
   const newPosition = editor.position.with(editor.position.line + 1, 0);
   editor.editor.edit((editBuilder) => {
     editBuilder.insert(newPosition, AIResponse + "\n");
@@ -40,7 +87,7 @@ function outputCode(editor: Editor, AIResponse: string) {
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "zen.getCurrentLineContent",
-    () => {
+    async () => {
       const editor = getEditor();
       // Get the active editor
       if (
@@ -48,7 +95,7 @@ export function activate(context: vscode.ExtensionContext) {
         !["	"].includes(editor?.lineContent) &&
         editor?.lineContent.length > 0
       ) {
-        const AIResponse = askAI(editor?.lineContent);
+        const AIResponse = await askAI(editor?.lineContent);
         outputCode(editor, AIResponse);
       }
     }
